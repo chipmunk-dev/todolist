b51c1b78cef3b1fcc2df5f2b279f0fe36074a227
chipmunk-dev <devquokkajeong@gmail.com>
2023-03-14 23:51:46 +0900

    feat: create controller

---
+++ src/controller/todo.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+
+export const getList = (req: Request, res: Response) => {};
+export const create = (req: Request, res: Response) => {};
+export const update = (req: Request, res: Response) => {};
+export const remove = (req: Request, res: Response) => {};
