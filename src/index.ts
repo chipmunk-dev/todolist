060b951235eab494f5f0f596cf061446044f5519
chipmunk-dev <devquokkajeong@gmail.com>
2023-03-14 22:21:13 +0900

    feat: apply middleware
    - cors
    - cookie-parser
    - helmet
    - morgan

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
-import express from 'express';
-
-import { config } from '../config';
+// global
+import express, { json } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+// local
 import './lib/db';
+import { config } from '../config';
 
 const app = express();
 const port = config.port;
+const corsOptions: cors.CorsOptions = {
+	origin: config.cors.origin,
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	optionsSuccessStatus: 200,
+	credentials: true,
+};
 
-app.use(express.json());
-app.use(express.raw({ type: 'application/vnd.custom-type' }));
-app.use(express.text({ type: 'text/html' }));
-
-app.get('/', async (req, res) => {
-	res.json({ message: 'Please visit /countries to view all the countries' });
-});
+app.use(json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use(morgan(config.morgan));
+app.use(helmet());
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
