2220371311c570fa4fdc95fd6503de60257664c5
chipmunk-dev <devquokkajeong@gmail.com>
2023-03-16 01:48:37 +0900

    feat: create todo feature

--- src/controller/todo.ts
+++ src/controller/todo.ts
@@ -1,6 +1,26 @@
 import { Request, Response } from 'express';
 
-export const getList = (req: Request, res: Response) => {};
-export const create = (req: Request, res: Response) => {};
-export const update = (req: Request, res: Response) => {};
-export const remove = (req: Request, res: Response) => {};
+import * as todoRepository from '../data/todo';
+import { TodoInput, TodoOutput } from '../interface/todo.inteface';
+
+export const getList = async (req: Request, res: Response) => {
+	const data = await todoRepository.findAll();
+	return res.status(200).json({ data });
+};
+export const create = async (req: Request, res: Response) => {
+	const { contents } = req.body;
+	const todo: TodoInput = { contents, complete: false };
+	const createTodo = await todoRepository.create(todo);
+	return res.status(201).json({ todoId: createTodo.id });
+};
+export const update = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const { contents, complete } = req.body;
+	const updateTodo = await todoRepository.update({ id, contents, complete });
+	return res.status(200).json({ todoId: updateTodo.id });
+};
+export const remove = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	await todoRepository.deleteById(id);
+	return res.sendStatus(204);
+};
