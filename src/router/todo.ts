437c397e8c8fa41420b966968449e9e59abe1947
chipmunk <107650362+chipmunk-dev@users.noreply.github.com>
2023-03-17 06:48:11 +0900

    feat: Change the status to be covered as well (#10)

--- src/router/todo.ts
+++ src/router/todo.ts
@@ -7,7 +7,7 @@ const { checkContents, checkComplete } = todoValidation;
 
 route.get('/', todoController.getList);
 route.post('/', [checkContents, validationErrorCheck], todoController.create);
-route.put('/:id', [checkContents, validationErrorCheck], todoController.update);
+route.put('/:id', [checkContents, checkComplete, validationErrorCheck], todoController.update);
 route.delete('/:id', todoController.remove);
 
 export default route;
