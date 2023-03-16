349c60d81208ecc4c11d658bdb1ad442fd2351f1
chipmunk <107650362+chipmunk-dev@users.noreply.github.com>
2023-03-16 22:48:29 +0900

    feat: apply validation to Todo domain (#8)

--- src/router/todo.ts
+++ src/router/todo.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import * as todoController from '../controller/todo';
+import { todoValidation, validationErrorCheck } from '../middleware/validation';
 
 const route = express.Router();
+const { checkContents, checkComplete } = todoValidation;
 
 route.get('/', todoController.getList);
-route.post('/', todoController.create);
-route.put('/:id', todoController.update);
+route.post('/', [checkContents, validationErrorCheck], todoController.create);
+route.put('/:id', [checkContents, validationErrorCheck], todoController.update);
 route.delete('/:id', todoController.remove);
 
 export default route;
